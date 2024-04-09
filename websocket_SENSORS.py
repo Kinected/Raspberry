d1001c2fdb4c1872123e6366878e1213f1649aec
Sam-Sponcy <93118296+Sam-Sponcy@users.noreply.github.com>
2024-04-09 15:28:20 +0200

    sensors_works

--- websocket_SENSORS.py
+++ websocket_SENSORS.py
@@ -2,12 +2,17 @@ import asyncio
 import json
 import websockets
 import Adafruit_DHT
+import board
+from adafruit_tsl2561 import TSL2561
 
 # Spécifiez le type de capteur (DHT11 ou DHT22) et le numéro de broche GPIO
 capteur = Adafruit_DHT.DHT11
 broche = 18
 
-lum = 0
+# Initialisation du capteur TSL2561
+i2c = board.I2C()  # Cr�e l'objet I2C
+tsl2561 = TSL2561(i2c)
+
 
 async def send_temperature():
     uri = "ws://172.20.10.3:8000/ws/sensors"  # Remplacez par l'URL WebSocket appropriée
@@ -16,19 +21,20 @@ async def send_temperature():
             async with websockets.connect(uri) as websocket:
                 while True:
                     humidite, temperature = Adafruit_DHT.read_retry(capteur, broche)
-                    lum =+ 1
-                    if humidite is not None and temperature is not None and lum is not None:
+                    lumiere = tsl2561.lux
+                    if humidite is not None and temperature is not None and lumiere is not None:
                         sensors_data = {
                             'temperature': temperature,
                             'humidity': humidite,
-                            'luminosity': lum
+                            'luminosity': round(lumiere)
+
                         }
                         sensors_json = json.dumps(sensors_data)
                         await websocket.send(sensors_json)
                     else:
                         print('Échec de la lecture du capteur. Réessayer!')
                     await asyncio.sleep(2)  # Attendre 2 secondes avant d'envoyer la prochaine température
-                    print('Température={0:0.1f}*C  Humidité={1:0.1f}% Luminosité={2:0.1f}'.format(temperature, humidite, lum))
+                    print('Température={0:0.1f}*C  Humidité={1:0.1f}% Luminosité={2:0.1f}'.format(temperature, humidite, lumiere))
         except websockets.exceptions.ConnectionClosedError:
             print("La connexion WebSocket a été fermée. Tentative de reconnexion...")
             await asyncio.sleep(5)  # Attendre 5 secondes avant de tenter une reconnexion
